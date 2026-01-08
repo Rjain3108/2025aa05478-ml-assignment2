5b0a8f6d7338a0088a5311cc92e74afdb8380db4
Rishabh Jain <Rishabh.jain@discounttire.com>
2026-01-08 06:41:32 +0530

    Added app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ st.text("Select the model using that you want to predict some result")
 model = st.selectbox("Select a model:", ['Logistics Regression', 'Decision Tree', 'kNN', 'Naive Bayes', 'Random Forest', 'XGBoost'])
 dataType = st.selectbox("Select a dataType:", ['file','Manually data enter'])
 
+model2 = st.selectbox("Select a model:", ['Logistics Regression', 'Decision Tree', 'kNN', 'Naive Bayes', 'Random Forest', 'XGBoost'])
+
 
 # A button that displays text when clicked
 if st.button("Calculate"):
-    st.write("Predicting value using model " + model + '.')
+    st.write("Predicting value using model " + model + '.' + model2)
